d35f16e29faf80177b45232803e041b264510454
jcr <jcr@prueba.com>
2023-09-21 11:24:55 +0200

    21-09-2023

---
+++ CodigosClase/Prog 1/Clase21-09-2023.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio 1</title>
+</head>
+<body>
+<!--comentario XML-->
+
+    <?php 
+    /**Ejercicio: Iterar el array $a y visualizarlo en formato tabla
+     * En cada linea tendremos tres campos: clave, tipo y valor
+     */
+    $a= [0=>"false", 1=>222, 2=>true, 7=>111, "hola"=>" que tal?"]; 
+
+    $a []="otro";
+
+    $t="<html><table border=1>\n";//Importante abrir y cerrar un html para la tabla
+    $t.="<tr>\n";
+    $t.="<th>Clave</th>\n";
+    $t.="<th>Tipo</th>\n";
+    $t.="<th>Valor</th>\n";
+    //Podriamos haber hecho tambien echo "<html><table border='1'> en lugar de meterlo en una variable $t"
+
+    foreach($a as $k => $v){
+        $tipo =gettype($v);
+        $t.="<tr>
+        \n<td>$k</td><td>$tipo</td><td>$v</td>\n
+        </tr>"; //No podemos meter el gettype a lo loco, es mejor meterlo antes en una variable. Podemos meterlo escribiento .gettype($v). en lugar de $tipo
+
+    }
+    //$t.="<tr>"; //$t=$t."<tr>"; Tambien se podría poner así
+
+    $t.="</table></html>";
+    echo $t;
+    unset ($a[1]);//Aunque aqui borremos el elemento 1, al no haber persistencia, al volver a cargar el programa el array volverá a estar entero
+    
+    ?>
+    
+</body>
+</html>
---
+++ CodigosClase/Prog 1/ej1.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+    <!-- Concatena dos cadenas con el operador punto (.) e imprimir su resultado.-->
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ejercicio_1</title>
+</head>
+<body>
+Página 7 del pdf php con ejemplos
+    <?php 
+    //Página 7 del pdf php con ejemplos
+    /*
+    Varias lineas 
+    de comentario
+    */
+    $ini = "Hola ";
+    $fin = " a todos";
+    $todo = "<br/>".$ini.$fin; //"." es el operador para concatenar strings
+    echo $todo;
+    //echo "<br/>.$ini.$fin;"
+    ?>
+    
+</body>
+</html>
