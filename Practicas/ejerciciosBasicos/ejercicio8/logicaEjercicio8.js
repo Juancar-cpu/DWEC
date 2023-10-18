d739ece9ac286bfd35fd0a5854af4c1c55f8ec74
Juancar-cpu <116200095+Juancar-cpu@users.noreply.github.com>
2023-10-18 10:07:58 +0200

    Update logicaEjercicio8.js

--- Practicas/ejerciciosBasicos/ejercicio8/logicaEjercicio8.js
+++ Practicas/ejerciciosBasicos/ejercicio8/logicaEjercicio8.js
@@ -1,1 +1,9 @@
+//Será un bucle for dentro de otro
 
+for(let v=0;v<=10;v++){//Vamos iterando entre tablas
+
+for(let i=0;i<=10;i++){//Desarrollamos cada tabla
+    console.log(`${v} X ${i} = ${v*i}`);
+}
+console.log(" ");//para dejar espacio entre las tablas, más claridad
+}
