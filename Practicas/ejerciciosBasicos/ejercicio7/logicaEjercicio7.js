29d5115bc9ddebdbff6ca8a878009aae9197a12e
Juancar-cpu <116200095+Juancar-cpu@users.noreply.github.com>
2023-10-18 10:01:49 +0200

    Update logicaEjercicio7.js

--- Practicas/ejerciciosBasicos/ejercicio7/logicaEjercicio7.js
+++ Practicas/ejerciciosBasicos/ejercicio7/logicaEjercicio7.js
@@ -1,1 +1,8 @@
+//Primero comprobamos que los dos numeros dados por el usuario son realmente numeros
+do{
+    var numero = parseInt(prompt("Introduce el primer numero"));
+}while(isNaN(numero))
 
+for(let i=0;i<=10;i++){
+    console.log(`${numero} X ${i} = ${numero*i}`);
+}
